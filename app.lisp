ece3ecca69ddb2fc6a8334da9bf2772e65f32945
Rajasegar Chandran <rajasegar.c@gmail.com>
2021-08-02 17:14:15 +0530

    chore: add fetch more and some ux tweaks

--- app.lisp
+++ app.lisp
@@ -19,6 +19,17 @@
       "direction-l"
       "direction-r"))
 
+(defvar *months* '("Jan" "Feb" "Mar" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec"))
+
+(djula:def-filter :format-date (val)
+  (let ((year (subseq val 0 4))
+        (month (subseq val 5 7))
+        (date (subseq val 8 10)))
+    (concatenate 'string date " " (nth (1- (parse-integer month)) *months*) " " year)))
+
+(defun increment-page (page)
+  (1+ (parse-integer page)))
+
 (defvar *app* (make-instance 'ningle:app))
 
 (setf (ningle:route *app* "/")  (render #P"index.html"))
@@ -27,7 +38,14 @@
                     :method :POST)
       #'(lambda (params)
           (let* ((user (cdr (assoc "user" params :test #'string=)))
-                 (repos (cl-json:decode-json-from-string (dex:get (concatenate 'string "https://api.github.com/users/" user "/repos")))))
-          (render #P"_timeline.html" (list :repos repos)))))
+                 (repos (cl-json:decode-json-from-string (dex:get (concatenate 'string "https://api.github.com/users/" user "/repos?sort=created&direction=desc")))))
+          (render #P"_timeline.html" (list :repos repos :user user)))))
+
+(setf (ningle:route *app* "/more")
+      #'(lambda (params)
+          (let* ((user (cdr (assoc "user" params :test #'string=)))
+                 (page (cdr (assoc "page" params :test #'string=)))
+                 (repos (cl-json:decode-json-from-string (dex:get (concatenate 'string "https://api.github.com/users/" user "/repos?sort=created&direction=desc&page=" page)))))
+          (render #P"_timeline-secondary.html" (list :repos repos :user user :page (increment-page page))))))
 
 *app*
